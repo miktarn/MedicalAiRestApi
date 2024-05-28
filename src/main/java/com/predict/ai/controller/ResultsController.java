cfe3f804abece8230bf045b1b5dc96c7010590e1
JadeRowan <rougesoul.m@gmail.com>
2024-05-28 16:33:35 +0300

    Added Cross Origin

--- src/main/java/com/predict/ai/controller/ResultsController.java
+++ src/main/java/com/predict/ai/controller/ResultsController.java
@@ -5,11 +5,13 @@ import com.predict.ai.service.ResultService;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/results")
 @AllArgsConstructor
--- src/main/java/com/predict/ai/controller/UserController.java
+++ src/main/java/com/predict/ai/controller/UserController.java
@@ -5,12 +5,14 @@ import com.predict.ai.model.dto.request.SaveUserDto;
 import com.predict.ai.service.UserService;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/users")
 @AllArgsConstructor
